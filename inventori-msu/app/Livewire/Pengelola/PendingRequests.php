d06deaafc718a4408018b0c4ca053003f913c6e4
Kruwpuck <ihabhasanainakmal0409@gmail.com>
2025-11-13 23:36:26 +0700

    tambah struktur livewire

---
+++ inventori-msu/app/Livewire/Pengelola/PendingRequests.php
@@ -0,0 +1,13 @@
+<?php
+
+namespace App\Livewire\Pengelola;
+
+use Livewire\Component;
+
+class PendingRequests extends Component
+{
+    public function render()
+    {
+        return view('livewire.pengelola.pending-requests');
+    }
+}
