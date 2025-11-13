1a09c1d048cf4fdde81eabc799fb81a31ca5f7bc
Kruwpuck <ihabhasanainakmal0409@gmail.com>
2025-11-13 23:36:26 +0700

    tambah struktur livewire

---
+++ inventori-msu/app/Livewire/Catalogue/DateRangePicker.php
@@ -0,0 +1,13 @@
+<?php
+
+namespace App\Livewire\Catalogue;
+
+use Livewire\Component;
+
+class DateRangePicker extends Component
+{
+    public function render()
+    {
+        return view('livewire.catalogue.date-range-picker');
+    }
+}
---
+++ inventori-msu/app/Livewire/Catalogue/InventoryCard.php
@@ -0,0 +1,13 @@
+<?php
+
+namespace App\Livewire\Catalogue;
+
+use Livewire\Component;
+
+class InventoryCard extends Component
+{
+    public function render()
+    {
+        return view('livewire.catalogue.inventory-card');
+    }
+}
