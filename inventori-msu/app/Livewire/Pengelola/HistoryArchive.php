1e9b1cfc9a3e60b12f2cc22d85c5b33ab0af69c4
Kruwpuck <ihabhasanainakmal0409@gmail.com>
2025-11-13 23:36:26 +0700

    tambah struktur livewire

---
+++ inventori-msu/app/Livewire/Pengelola/HistoryArchive.php
@@ -0,0 +1,13 @@
+<?php
+
+namespace App\Livewire\Pengelola;
+
+use Livewire\Component;
+
+class HistoryArchive extends Component
+{
+    public function render()
+    {
+        return view('livewire.pengelola.history-archive');
+    }
+}
