35936230b3c234395f645a0d0e74d66cbaf96314
arinanabila <areenrhmania@gmail.com>
2025-12-18 11:24:15 +0700

    update

--- inventori-msu/resources/views/pengurus/dashboard.blade.php
+++ inventori-msu/resources/views/pengurus/dashboard.blade.php
@@ -24,7 +24,7 @@
 
     <div class="user-desktop" onclick="toggleUserDropdown()">
         <div class="user-info-box">
-            <img src="{{ asset('Assets/logo.png') }}" class="user-photo">
+            <div class="user-photo" style="background-image: url('{{ asset('Assets/logo.png') }}'); background-size: cover; background-position: center;"></div>
             <div>
                 <strong>pengurus</strong><br>
                 Pengurus Side
