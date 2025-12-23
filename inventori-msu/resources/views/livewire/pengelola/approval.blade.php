9a82e69543b9553b7fb06a18038768a572442ad8
arinanabila <areenrhmania@gmail.com>
2025-12-23 09:15:36 +0700

    ganti font 2

--- inventori-msu/resources/views/livewire/pengelola/approval.blade.php
+++ inventori-msu/resources/views/livewire/pengelola/approval.blade.php
@@ -390,6 +390,7 @@
   {{-- =========================
   HISTORY SECTION
   ========================= --}}
+  <div class="mb-5">
     <section class="text-center mb-4">
       <p class="mb-1 fw-semibold" style="letter-spacing:.06em;color:#2a6a55">ARSIP DATA</p>
       <h2 class="fw-bolder text-uppercase" style="color:#32435a">Riwayat Keputusan</h2>
